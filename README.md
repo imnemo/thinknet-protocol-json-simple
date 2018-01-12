0e558742439600e3d1511c5b400fb5ff8cff21a4
nemo <nemo@oncoding.me>
2018-01-12 15:11:37 +0800

    core codes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # thinknet-protocol-json-simple
 simple JSON data protocol for ThinkNet, a easy to use and koa.js or express.js middleware style net programming framework
+
+# Usage
+
+check [thinknet](/imnemo/thinknet) for more details
---
+++ index.js
@@ -0,0 +1,40 @@
+const debug = require('debug')('thinknet')
+const {Protocol} = require('../thinknet')
+
+class ProtocolJsonSimple extends Protocol {
+  constructor(opt = {}) {
+    super(opt)
+    this.eol = (opt.eol === undefined ? '\r\n' : opt.eol)
+  }
+  parse(buf, opt = {}) {
+    if(opt.type === 'object')
+      return [].push(buf)
+    if(opt.type === 'array')
+      return buf
+    //默认buf的数据类型是TCP传输的Buffer
+    if(!Buffer.isBuffer(buf)){
+      debug(`Error: data type invalid!`)
+      return []
+    }
+    let dataStrArry = buf
+                  .toString('utf8')
+                  .trimRight('\r\n')
+                  .split('\r\n')
+    let dataObjArry = []
+    dataStrArry.forEach((dataStr) => {
+      let dataObj
+      try{
+        dataObj = JSON.parse(dataStr)
+      }catch(e){
+        return
+      }
+      dataObjArry.push(dataObj)
+    })
+    return dataObjArry
+  }
+  build(data) {
+    return JSON.stringify(data) + this.eol
+  }
+}
+
+module.exports = ProtocolJsonSimple
